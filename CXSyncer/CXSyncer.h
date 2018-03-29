9ac8365af88ea78381f182f08515faf9a05a6808
cunqi <cx363@nyu.edu>
2018-03-28 21:30:52 -0400

    Initial Commit

---
+++ CXSyncer/CXSyncer.h
@@ -0,0 +1,19 @@
+//
+//  CXSyncer.h
+//  CXSyncer
+//
+//  Created by Cunqi Xiao on 3/28/18.
+//  Copyright © 2018 Cunqi Xiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CXSyncer.
+FOUNDATION_EXPORT double CXSyncerVersionNumber;
+
+//! Project version string for CXSyncer.
+FOUNDATION_EXPORT const unsigned char CXSyncerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CXSyncer/PublicHeader.h>
+
+
